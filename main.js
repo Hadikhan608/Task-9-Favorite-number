470c7c525410b3d9e04904cbeb8a6a3a02f842b2
Abdul Hadi <161719851+Hadikhan608@users.noreply.github.com>
2024-05-05 22:58:18 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+//Fvorite number: Store your fav num in a variable then create a message revealing your favourite num. 
+//print that message.
+var favouritenumber = 20;
+console.log("My favorite number is".concat(favouritenumber));
---
+++ main.ts
@@ -0,0 +1,4 @@
+//Fvorite number: Store your fav num in a variable then create a message revealing your favourite num. 
+//print that message.
+let favouritenumber = 20;
+console.log(`My favorite number is${favouritenumber}`);
